029668112e443b95451e3c74ec7f8e62d900421e
Anton Voytov <august-ra@ya.ru>
2024-02-08 02:37:25 +0300

    done

--- js/hw-3.js
+++ js/hw-3.js
@@ -29,7 +29,7 @@ export function printEN(root) {
 
     const arr = [0, 10, -3, 2]
 
-    root.innerHTML += `<h2>Task №2</h2>`
+    root.innerHTML += `<h2>Task #2</h2>`
     root.innerHTML += `<p>Checking that the range <b>(0; 10)</b> includes numbers from a set.</p>`
 
     for (const item of arr) {
@@ -59,7 +59,7 @@ export function printEN(root) {
     a = "2";
     b = "3";
 
-    root.innerHTML += `<h2>Task №4</h2>`
+    root.innerHTML += `<h2>Task #4</h2>`
     root.innerHTML += `<p>There was two strings <b>${a}</b> и <b>${b}</b> but the summary is <b>${Number(a) + Number(b)}</b>.</p>`
 
     // task #05
@@ -91,7 +91,7 @@ export function printEN(root) {
             answer = null
     }
 
-    root.innerHTML += `<h2>Task №5</h2>`
+    root.innerHTML += `<h2>Task #5</h2>`
 
     if (answer === null)
         root.innerHTML += `<p>There isn't month with <b>${monthNumber}</b> number in a year.</p>`
@@ -101,7 +101,7 @@ export function printEN(root) {
     // task #06
 
     root.innerHTML += `<h2>Task #6</h2>`
-    root.innerHTML += `<p>There is my games' page. <a href="index.html">Tap it!</a></p>`
+    root.innerHTML += `<p>There is my games' page. <a href="../index.html">Tap it!</a></p>`
 }
 
 export function printRU(root) {
@@ -109,7 +109,7 @@ export function printRU(root) {
 
     // task #01
 
-    root.innerHTML += `<h2>Задача #1</h2>`
+    root.innerHTML += `<h2>Задача №1</h2>`
 
     if (!confirm)
         confirm = prompt(`Введите пароль.`)
@@ -145,7 +145,7 @@ export function printRU(root) {
     else
         answer = "Неверно!"
 
-    root.innerHTML += `<h2>Задача #3</h2>`
+    root.innerHTML += `<h2>Задача №3</h2>`
     root.innerHTML += `<p>Для значений <b>${d}</b> и <b>${e}</b> система ответила <b>"${answer}"</b>.</p>`
 
     // task #04
@@ -199,5 +199,5 @@ export function printRU(root) {
     result = a % b
 
     root.innerHTML += `<h2>Задача №6</h2>`
-    root.innerHTML += `<p>Переход на страницу с играми <a href="index.html">здесь!</a></p>`
+    root.innerHTML += `<p>Переход на страницу с играми <a href="../index.html">здесь!</a></p>`
 }
---
+++ js/hw-4.js
@@ -0,0 +1,270 @@
+import * as common from "./common.js"
+
+let arr
+let delimiter
+
+export function printEN(root) {
+    root.innerHTML = `<h1 class="heading">There's a page for <b>JS</b>.</h1><br>`
+
+    // task #1
+
+    const double = [1, 2]
+    arr = ['—', 'Doctor', 'any things ', 'I say', 'I\'m saying ', 'twice', '.<br>', '—', 'Hello!']
+    delimiter = ', '
+    let index = 0
+
+    root.innerHTML += `<h2>Task #1</h2>`
+    root.innerHTML += `<p>`
+
+    for (const text of arr) {
+        ++index
+
+        say_twice:
+            for (const step of double) {
+                root.innerHTML += `${text}`
+
+                if (index === 6 && step === 2)
+                    delimiter = ''
+                else if (index === 8)
+                    delimiter = ' '
+
+                if ([1, 3, 5, 7, 8].includes(index))
+                    break say_twice
+                else
+                    root.innerHTML += delimiter
+            }
+    }
+
+    root.innerHTML += `</p>`
+
+    // task #2
+
+    delimiter = ''
+
+    root.innerHTML += `<h2>Task #2</h2>`
+    root.innerHTML += `<p>`
+
+    arr = ['one', 'two', 'three', 'four', 'five']
+
+    for (let index = 0; index < 5; ++index) {
+        root.innerHTML += delimiter
+        root.innerHTML += arr[index]
+
+        delimiter = ', '
+    }
+
+    root.innerHTML += `</p>`
+
+    // task #3
+
+    delimiter = ''
+
+    root.innerHTML += `<h2>Task #3</h2>`
+    root.innerHTML += `<p>`
+
+    for (let index = 7; index <= 22; ++index) {
+        root.innerHTML += delimiter
+        root.innerHTML += index
+
+        delimiter = ', '
+    }
+
+    root.innerHTML += `</p>`
+
+    // task #4
+
+    const data = {
+        'Bob': 200,
+        'Jack': 300,
+        'John': 400,
+    }
+
+    root.innerHTML += `<h2>Task #4</h2>`
+    root.innerHTML += `<p>`
+
+    for (const key in data) {
+        root.innerHTML += `${key} — salary is $${data[key]};<br>`
+    }
+
+    root.innerHTML += `<i>Be better than those people! Be like Henry.</i>`
+    root.innerHTML += `</p>`
+
+    // task #5
+
+    let num = 1000
+    let counter = 0
+
+    while (num >= 50) {
+        ++counter
+        num /= 2
+    }
+
+    root.innerHTML += `<h2>Task #5</h2>`
+    root.innerHTML += `<p>The system makes <b>${counter} divisions</b> to get <b>${num}</b>.</p>`
+
+    // task #6
+
+    root.innerHTML += `<h2>Task #6</h2>`
+
+    new Schedule().printFridays(root, 'en')
+}
+
+export function printRU(root) {
+    root.innerHTML = `<h1 class="heading">ВНИМАНИЕ! Страница исполнения <b>JS</b>.</h1><br>`
+
+    // task #01
+
+    const double = [1, 2]
+    arr = ['—', 'Доктор', 'всё, что я бы ', 'ни говорил', 'я повторяю ', 'по два раза', '.<br>—', 'Привет!']
+    delimiter = ', '
+    let index = 0
+
+    root.innerHTML += `<h2>Задача №1</h2>`
+    root.innerHTML += `<p>`
+
+    for (const text of arr) {
+        ++index
+
+        say_twice:
+            for (const step of double) {
+                root.innerHTML += `${text}`
+
+                if (index === 6 && step === 2)
+                    delimiter = ''
+                else if (index === 8)
+                    delimiter = ' '
+
+                if ([1, 3, 5, 7].includes(index))
+                    break say_twice
+                else
+                    root.innerHTML += delimiter
+            }
+    }
+
+    root.innerHTML += `</p>`
+
+    // task #02
+
+    delimiter = ''
+
+    root.innerHTML += `<h2>Задача №2</h2>`
+    root.innerHTML += `<p>`
+
+    arr = ['раз', 'два', 'три', 'четыре', 'пять']
+
+    for (let index = 0; index < 5; ++index) {
+        root.innerHTML += delimiter
+        root.innerHTML += arr[index]
+
+        delimiter = ', '
+    }
+
+    root.innerHTML += `</p>`
+
+    // task #03
+
+    delimiter = ''
+
+    root.innerHTML += `<h2>Задача №3</h2>`
+    root.innerHTML += `<p>`
+
+    for (let index = 7; index <= 22; ++index) {
+        root.innerHTML += delimiter
+        root.innerHTML += index
+
+        delimiter = ', '
+    }
+
+    root.innerHTML += `</p>`
+
+    // task #04
+
+    const data = {
+        'Коля': 200,
+        'Вася': 300,
+        'Петя': 400,
+    }
+
+    root.innerHTML += `<h2>Задача №4</h2>`
+    root.innerHTML += `<p>`
+
+    for (const key in data) {
+        root.innerHTML += `${key} — зарплата ${data[key]} долларов<br>`
+    }
+
+    root.innerHTML += `<i>Не будь, как эти трое! Будь, как Михаил.</i>`
+    root.innerHTML += `</p>`
+
+    // task #05
+
+    let num = 1000
+    let counter = 0
+
+    while (num >= 50) {
+        ++counter
+        num /= 2
+    }
+
+    const units = common.calcUnits(counter, 'делений', 'деление', 'деления')
+
+    root.innerHTML += `<h2>Задача №5</h2>`
+    root.innerHTML += `<p>Система произвела <b>${counter} ${units}</b> и получила <b>${num}</b>.</p>`
+
+    // task #06
+
+    root.innerHTML += `<h2>Задача №6</h2>`
+
+    new Schedule().printFridays(root, 'ru')
+}
+
+
+class Schedule {
+    constructor(month) {
+        this.date = new Date()
+        this.year = this.date.getFullYear()
+        this.month = month ? month - 1 : this.date.getMonth()
+        this.day = 0
+        this.dayOfWeek = -1
+        this.lastDayOfMonth = new Date(this.year, this.month + 1, this.day).getDate()
+    }
+
+    printFridays = (root, lang) => {
+        do {
+            ++this.day
+
+            this.date = new Date(this.year, this.month, this.day)
+            this.dayOfWeek = this.date.getDay()
+        }
+        while (this.dayOfWeek !== 5)
+
+        while (this.day <= this.lastDayOfMonth) {
+            if (lang === 'ru') {
+                root.innerHTML += `<h5>${this.date.toLocaleDateString('ru-RU')}</h5>`
+                root.innerHTML += `<p>Сегодня ${this.day}-е число. Это пятница — садись делать отчёт.</p>`
+            }
+            else {
+                root.innerHTML += `<h5>${this.date.toLocaleDateString('en-GB')}</h5>`
+                root.innerHTML += `<p>It's a friday the ${this.day}th. Do the report for your boss.</p>`
+            }
+
+            this.day += 7
+            this.date = new Date(this.year, this.month, this.day)
+        }
+    }
+
+}
+
+// task #7
+
+const circle = {
+    radius: 5,
+    getArea() {
+        return Math.PI * this.radius ** 2
+    },
+    getPerimeter() {
+        return 2 * Math.PI * this.radius
+    },
+}
+
+console.log(circle.getArea())
+console.log(circle.getPerimeter())
