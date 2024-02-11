5ce59380ab9ed2e254ea324b6a1b20d5b7c72a8b
Anton Voytov <august-ra@ya.ru>
2024-02-11 19:16:47 +0300

    hotfix

--- main.js
+++ main.js
@@ -20,17 +20,25 @@ document.getElementById("game_3").addEventListener("click", () => jumpToSection(
 
 // games
 
-document.getElementById("start_1").addEventListener('click', () => {
-    let month = Number(prompt('Введите номер месяца, чтобы узнать время года.'))
+document.getElementById("start_1").addEventListener("click", () => {
+    const text = prompt("Введите номер месяца, чтобы узнать время года.")
+    const month = Number(text)
+    const sentence = "Не смогу определить сезон."
 
-    if (month === 1 || month === 2 || month === 12)
-        console.log('Время года — Зима.')
+    if (text === "")
+        console.log(`Вы ничего не ввели. ${sentence}`)
+    else if (isNaN(month))
+        console.log(`«${text}»\nВы ввели не число. ${sentence}`)
+    else if (month < 0)
+        console.log(`Вы ввели число меньшее, чем 0. ${sentence}`)
+    else if (month === 1 || month === 2 || month === 12)
+        console.log("Время года — Зима.")
     else if (month >= 3 && month <= 5)
-        console.log('Время года — Весна.')
+        console.log("Время года — Весна.")
     else if (month >= 6 && month <= 8)
-        console.log('Время года — Лето.')
+        console.log("Время года — Лето.")
     else if (month >= 9 && month <= 11)
-        console.log('Время года — Осень.')
+        console.log("Время года — Осень.")
     else
-        console.log('Введите НОМЕР месяца')
+        console.log(`${text}»\nВы ввели число большее, чем 12. ${sentence}`)
 })
